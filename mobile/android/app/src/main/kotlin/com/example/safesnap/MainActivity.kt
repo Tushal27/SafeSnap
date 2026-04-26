507a9774f7387de1f039c2f07ff7855b0ba12b5b
Tushal <rajtushal74@gmail.com>
2026-04-26 18:20:53 +0530

    added models

---
+++ mobile/android/app/src/main/kotlin/com/example/safesnap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.safesnap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mobile/safesnap/android/app/src/main/kotlin/com/safesnap/safesnap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.safesnap.safesnap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
